5cb233a250a7a0e6a40a4e62a7e560534dcc7d39
ronilbhatia <ronilbhatia@gmail.com>
2018-07-09 15:45:57 -0700

    Complete First Routes & Controllers Project

--- routes/app/controllers/users_controller.rb
+++ routes/app/controllers/users_controller.rb
@@ -1,21 +1,46 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I am the users' index"
+    @users = User.all
+    render json: @users
   end
 
   def create
-    render json: params
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: 201
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+
+  end
+
+  def user_params
+    params.require(:user).permit([:name, :email])
   end
 
   def show
-    render json: params
+    @user = User.find(params[:id])
+    render json: @user, status: 200
   end
 
   def update
-    render json: params
+    if User.exists?(params[:id])
+      @user = User.find(params[:id])
+      @user.update(user_params)
+      render json: @user, status: 201
+    else
+      render plain: 'User does not exist', status: 404
+    end
   end
 
   def destroy
-    render json: params
+    if User.exists?(params[:id])
+      @user = User.find(params[:id])
+      @user.destroy
+      # render plain: 'User destroyed'
+      render json: ['User destroyed:', @user], status: 200
+    else
+      render plain: 'User does not exist', status: 404
+    end
   end
 end
