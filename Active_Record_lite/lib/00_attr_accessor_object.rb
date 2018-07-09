38df07cfdf6a9c05f21e6c9f7039527e6ced881e
ronilbhatia <ronilbhatia@gmail.com>
2018-07-09 13:19:18 -0700

    Update attr_accessor_object to do the problem without using herdoc

--- Active_Record_lite/lib/00_attr_accessor_object.rb
+++ Active_Record_lite/lib/00_attr_accessor_object.rb
@@ -2,18 +2,15 @@ class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
     names.each do |name|
-      heredoc = <<-MAGIC_STRING
-        def #{name}
-          @#{name}
-        end
-        
-        def #{name}=(value)
-          @#{name} = value
-        end
-      MAGIC_STRING
-      class_eval heredoc
+      define_method(name) do
+        instance_variable_get("@#{name}")
+      end
+
+      define_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
+      end
+
+      # define_method("#{name}=")
     end
   end
 end
-
-
