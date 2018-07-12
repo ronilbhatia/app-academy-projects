711a24932751676132060754d21e84aa40fec294
ronilbhatia <ronilbhatia@gmail.com>
2018-07-12 14:08:25 -0700

    Create User and Session Controllers, and complete login/logout and authentication logic

--- music_app/app/controllers/application_controller.rb
+++ music_app/app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    return nil if session[:session_token].nil?
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!@current_user
+  end
+
+  def login!(user)
+    @current_user = user
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+  end
+
+  def logout!(user)
+    @current_user = nil
+    session[:session_token] = nil
+  end
 end
